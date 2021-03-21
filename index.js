ddaba251baadc5d95cb56672c300f9180662bd45
Darya van Tienhoven <daryaheleen@gmail.com>
2021-03-21 21:00:27 +0100

    combining Saskia's html & CSS and Samuel's JavaScript

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const button = document.getElementById("btn");
 const btnDiv = document.querySelector(".button");
 const floor = document.querySelector(".floor");
-const box = document.getElementById('box');
+const box = document.getElementById('reward');
 const container = document.getElementsByClassName("container");
+const body = document.getElementById("body") 
 
 
 const thePrince = () => {
     btnDiv.innerHTML = "";
-    box.innerHTML = "";
+    box.innerHTML = "Hooray! You did it! Now live happily ever after :)";
     button.classList.add("invisible")
     const newDiv = document.createElement('div');
     const princeIMG = document.createElement('img');
-    princeIMG.classList.add("smallPrince")
-    princeIMG.src = "https://webstockreview.net/images/scooby-doo-clipart-scared-5.png";
+    princeIMG.classList.add("prince-small")
+    princeIMG.src = "img/prince.png";
     newDiv.appendChild(princeIMG)
-    floor.appendChild(newDiv)
+    button.appendChild(newDiv)
+    body.classList.add("rainbow")
     setTimeout(() => {
-        princeIMG.classList.remove("smallPrince")
-        princeIMG.classList.add("prince");
+        princeIMG.classList.add("prince-big");
     }, 900)
 
 };
@@ -44,14 +45,14 @@ function moveButton() {
     if (cont < 10) {
         button.animate([
             { transform: "scale(0.8)", "left": randX + "px" },
-            { transform: "scale(1.2)", "top": 100 + "px" }
+            { transform: "scale(1.2)", "bottom": 100 + "px" }
         ], { duration: 2000 })
     } else if (cont == 10) {
         theMeme();
     } else {
         button.animate([
             { "left": randX + "px" },
-            { "top": 100 + "px" },
+            { "bottom": 100 + "px" },
 
         ], { duration: 5000 })
 
@@ -61,3 +62,5 @@ function moveButton() {
 button.addEventListener('mouseenter', moveButton);
 
 button.addEventListener('click', thePrince);
+
+// thePrince()
